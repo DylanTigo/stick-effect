bf7983cb3ca62f6f0aba82fe00ac16dace7d3a8f
DylanTigo <dylannoeltigomo@gmail.com>
2025-03-17 19:52:49 +0100

    feat: implement GSAP animations for homepage and product cards; add splitText utility function

--- app/page.tsx
+++ app/page.tsx
@@ -1,30 +1,133 @@
+"use client";
+
 import Card from "@/components/Card";
-import {products} from "@/data.js"
+import { products } from "@/data.js";
+import gsap from "gsap";
+import { splitText } from "@/utils";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  return (
-    <main className="p-3 text-slate-900 relative bg-slate-50">
-      <section className="h-[30vh] w-full overflow-hidden ">
-      </section>
-      <section>
-        <div className="border-t-2 border-t-slate-900/90 py-5 md:py-7 bg-slate-50">
-          <h1 className="text-[calc(42px+2vw)] md:text-[calc(48px+4vw)] leading-tight mb-1">New Collection.</h1>
-          <p className="text-sm font-thin maw-w-[80%] w-[400px] ml-1 lg:ml-2">See the world clearly: brand new stylish sunglasses with ultimate UV protection.</p>
-        </div>
-        <div className="relative border-y-2 border-y-slate-900/90 divide-y-2 divide-gray-900/90">
+  const main = useRef(null);
+  const title = useRef<HTMLHeadingElement>(null);
+  const subtitle = useRef<HTMLParagraphElement>(null);
+  const border = useRef(null);
+  const lastSection = useRef(null)
+
+  const timeline = useRef<gsap.core.Timeline | null>(null);
+
+  useGSAP(() => {
+    timeline.current = gsap.timeline({ paused: true });
+
+    const spans = title.current?.querySelectorAll("span");
+    if (spans) {
+      timeline.current
+        .to(spans, {
+          y: 0,
+          stagger: 0.01,
+          duration: 0.6,
+        })
+        .to(
+          subtitle.current,
           {
-            products.map((product, i) => (
-              <Card key={i} product={product}/>
-            ))
-          }
+            y: 0,
+            duration: 0.6,
+            opacity: 1,
+          },
+          "<+=0.4"
+        )
+        .to(
+          border.current,
+          {
+            delay: 0.2,
+            width: "100%",
+            ease: "power2.out",
+            duration: 0.8,
+          },
+          "<-=0.4"
+        );
+    }
+    timeline.current.play();
+  });
+
+  useGSAP(() => {
+    const tl = gsap.timeline({ paused: true });
+    tl.to(".border-item", {
+      width: "100%",
+      ease: "power2.out",
+      stagger: 0.1,
+      duration: 0.8,
+    }).to("p.flex span", {
+      y: 0,
+      duration: 0.6,
+      stagger: 0.01
+    }, "<+0.1");
+
+    gsap.to(lastSection.current, {
+      scrollTrigger: {
+        trigger: lastSection.current,
+        start: "30% 60%",
+        onEnter: () => {
+          tl.play();
+        },
+        onLeaveBack: () => {
+          tl.reverse();
+        },
+      }
+    })
+  }, { scope: lastSection});
+
+  return (
+    <main ref={main} className="p-3 text-slate-900 relative bg-slate-50">
+      <section className="h-[30vh] w-full overflow-hidden "></section>
+      <section>
+        <div className="py-5 md:py-7 bg-slate-50 relative">
+          <span
+            ref={border}
+            className="absolute inset-0 w-0 h-0.5 bg-slate-900/90"
+          ></span>
+          <div className="w-fit h-fit overflow-hidden">
+            <h1
+              ref={title}
+              className="text-[calc(42px+2vw)] md:text-[calc(48px+4vw)] leading-tight mb-1"
+            >
+              {splitText("New Collection.", "translate-y-full")}
+            </h1>
+          </div>
+          <div className="w-fit h-fit overflow-hidden">
+            <p
+              ref={subtitle}
+              className="text-sm font-thin w-[80%] max-w-[400px] ml-1 lg:ml-2 origin-top-left translate-y-7 opacity-0 "
+            >
+              See the world clearly: brand new stylish sunglasses with ultimate
+              UV protection
+            </p>
+          </div>
+        </div>
+        <div className="relative ">
+          {products.map((product, i) => (
+            <Card key={i} product={product} />
+          ))}
         </div>
       </section>
-      <section className="h-[80svh] flex justify-center items-center">
-        <p className="text-[calc(32px+2vw)] md:text-[calc(48px+4vw)] leading-tight mb-1">Choose your favorite</p>
+      <section ref={lastSection} className="h-[90svh] flex justify-center items-center relative">
+        <span
+          className="border-item absolute left-0 top-0 w-0 h-0.5 bg-slate-900/90"
+        />
+        <div className="w-fit h-fit overflow-hidden">
+          <p className="flex text-[calc(32px+2vw)] md:text-[calc(48px+4vw)] leading-tight mb-1">
+            {splitText("Make your choise", "translate-y-full")}
+          </p>
+        </div>
+        <span
+          className="border-item absolute left-0 bottom-0 w-0 h-0.5 bg-slate-900/90"
+        />
       </section>
-      <footer className="border-t-2 border-t-slate-900/90 text-center pt-2">
-       Dylan Noel
-      </footer>
+      <footer className="text-center pt-2">Dylan Noel</footer>
     </main>
   );
 }
--- components/Card.tsx
+++ components/Card.tsx
@@ -1,16 +1,86 @@
-import Image, { StaticImageData } from "next/image";
+'use client'
 
+import Image, { StaticImageData } from "next/image";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { splitText } from "@/utils";
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 type Product = {
   title: string;
   description: string;
   colors: string[];
   image: StaticImageData;
   price: number;
-}
+};
 
-const Card = ({ product } : { product : Product}) => {
+const Card = ({ product }: { product: Product }) => {
+
+  const card = useRef(null);
+  const tl = useRef<gsap.core.Timeline | null>(null);
+  const border = useRef(null);
+  const title = useRef<HTMLHeadingElement>(null);
+  const description = useRef<HTMLParagraphElement>(null);
+  const price = useRef(null)
+  const colorsGroup = useRef(null)
+
+  useGSAP(() => {
+    tl.current = gsap.timeline({ paused: true });
+    tl.current.to( border.current, {
+      delay: 0.2,
+      width: "100%",
+      ease: "power2.out",
+      duration: 0.8,
+    })
+    .to(".title-span", {
+      y: 0,
+      duration: 0.6,
+      stagger: 0.01
+    }, "<+0.1")
+    .to(description.current, {
+      y: 0,
+      duration: 0.6,
+      opacity: 1,
+    }, "<+=0.4")
+    .to(price.current, {
+      x: 0,
+      duration: 0.6,
+      opacity: 1
+    }, "1-=0.2")
+    .to(colorsGroup.current, {
+      y: 0,
+      opacity: 1,
+      duration: 0.6
+    }, "<+=0.4")
+  }, { scope: card })
+
+  useGSAP(() => {
+    gsap.to(card.current, {
+      y: 0,
+      opacity: 1,
+      duration: 1,
+      ease: "power2.out",
+      scrollTrigger: {
+        trigger: card.current,
+        start: "top bottom",
+        end: "bottom bottom",
+        toggleActions: "play none none reverse",
+        onEnter: () => {
+          tl.current?.play();
+        },
+        onLeaveBack: () => {
+          tl.current?.reverse();
+        }
+      },
+    })
+  })
+
   return (
-    <div className="bg-slate-50 grid grid-cols-12 gap-y-3 py-5">
+    <div ref={card} className="card relative opacity-0 bg-slate-50 grid grid-cols-12 gap-y-3 py-5 translate-y-40">
+      <span ref={border} className="absolute inset-0 w-0 h-0.5 bg-slate-900/90"></span>
       <div className="col-span-full md:col-span-4 rounded-md items-stretch overflow-hidden">
         <Image
           src={product.image}
@@ -22,24 +92,43 @@ const Card = ({ product } : { product : Product}) => {
       </div>
       <div className="col-span-full h-full md:col-span-8 flex flex-col justify-between md:ml-4">
         <div>
-          <div className="flex justify-between gap-4">
-            <h2 className="text-3xl sm:text-4xl w-[90%] line-clamp-2 sm:line-clamp-1">{product.title}</h2>
-            <div className="flex "><span className="text-3xl sm:text-4xl">{product.price}</span>$</div>
+          <div className="flex justify-between gap-3">
+            <div className="w-fit h-fit grow overflow-hidden">
+              <h2 ref={title} className="flex text-2xl sm:text-3xl lg:text-4xl w-[90%]">
+                {splitText(product.title, "title-span translate-y-full")}
+              </h2>
+            </div>
+
+            <div ref={price} className="flex opacity-0 translate-x-1/3">
+              <span className="flex text-2xl sm:text-3xl lg:text-4xl">{product.price.toFixed(2)}</span>
+              $
+            </div>
           </div>
-          <p className="text-sm font-thin mt-3 md:mt-5 lg:w-96 line-clamp-3">{product.description}</p>
+          <div className="w-fit h-fit overflow-hidden">
+            <p ref={description} className="text-sm font-thin mt-3 md:mt-5 lg:w-96 line-clamp-3 translate-y-7 opacity-0">
+              {product.description}
+            </p>
+          </div>
         </div>
 
         <div className="flex md:flex-col justify-between max-md:items-end gap-1.5 md:gap-3 mt-4 mb-1 grow">
-          <div className="space-y-1.5 text-lg">
+          <div ref={colorsGroup} className="space-y-1.5 text-lg opacity-0 translate-y-1/3">
             <div>Colors</div>
             <div className="flex gap-1.5">
               {product.colors.map((color, i) => (
-                <span key={color+i} style={{ background: color}} className="size-5 rounded-full border border-slate-900/20"></span>
+                <span
+                  key={color + i}
+                  style={{ background: color }}
+                  className="size-5 rounded-full border border-slate-900/20"
+                ></span>
               ))}
             </div>
           </div>
-          <a href="#" className="text-lg text-nowrap underline underline-offset-2">
-            Learn more
+          <a
+            href="#"
+            className="text-lg text-nowrap underline underline-offset-2"
+          >
+            Show more
           </a>
         </div>
       </div>
